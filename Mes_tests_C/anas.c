9067134413340cc4ee60a4cc0f0c5c4f489b80c9
Korabicca <korabicca.host@gmail.com>
2023-03-16 20:37:11 -0700

    generic

---
+++ Mes_tests_C/anas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+
+
+
+{
+	int NUM;
+       	printf("hello my world\nGive me a number: ");
+	scanf("%d",&NUM);
+	printf("You're number is : %d\n",NUM);
+	return 0;
+}
