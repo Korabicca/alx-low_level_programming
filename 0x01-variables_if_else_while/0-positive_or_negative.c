3ed603ddb5f1c09038b8147e1bf25e010f03d93d
Korabicca <korabicca.host@gmail.com>
2023-03-17 13:33:08 -0700

    generic

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,16 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 	        int n;
@@ -18,4 +25,3 @@ int main(void)
 						        else { printf("%d is negative\n",n);}
 							        return(0);
 }
-~                                                                                                            ~             
