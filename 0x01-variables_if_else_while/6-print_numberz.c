01181e7b05c858f35466e927b537c6fc5c159eb5
Korabicca <korabicca.host@gmail.com>
2023-03-17 21:42:39 -0700

    generic

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,13 @@
 * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
-  int i;
+int i;
 for (i = 48 ; i < 58 ; i++)
 {
 putchar(i);
 }
-putchar('\n');
-  
-return (0); 
+putchar('\n');  
+return (0);
 }
