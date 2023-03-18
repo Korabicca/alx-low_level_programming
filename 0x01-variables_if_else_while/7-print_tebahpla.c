bb5a7a8e663379bd6791213bc8c004b023e3d349
Korabicca <korabicca.host@gmail.com>
2023-03-17 22:18:05 -0700

    generic

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)/**
+	       * main - Entry point
+	       *
+	       * Return: Always 0 (Success)
+	       */
+/**Entry point
+*
+* Return: Always 0 (Success)
+*/  
+{
+int i;
+for(i = 122 ; i > 96 ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
